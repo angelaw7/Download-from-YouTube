36565f92f2b6881082e7dd8c8d1cac8701513edc
Angela <wangela472@gmail.com>
2021-12-26 22:14:56 -0500

    Fix overlapping labels when changing text

--- yt_gui.py
+++ yt_gui.py
@@ -1,24 +1,27 @@
 from tkinter import *
+from tkinter import filedialog
+
 from yt import download_video
 
 root = Tk()
 root.title("YouTube to mp4")
 # root.iconbitmap("")
 root.geometry("600x400")
+root.directory = "C:/Users/Downloads"
 
 
-success_text = Label(root, text="")
-error_text = Label(root, text="")
+def change_save_path():
+    root.directory = filedialog.askdirectory()
+    download_location.config(text=root.directory)
 
 
 def reset_messages():
-    global success_text, error_text
-    success_text.grid_remove()
-    error_text.grid_remove()
+    success_text.config(text="")
+    error_text.config(text="")
 
 
 def download_video_button():
-    global success_text, error_text
+    change_save_path()
     reset_messages()
 
     current = e.get()
@@ -30,17 +33,19 @@ def download_video_button():
     if res == "Invalid link" or res == "Download error":
         print("failed")
         error_type = res
-        error_text = Label(root, text="Error: " + error_type)
-        error_text.grid(row=6, column=0, columnspan=4)
+        error_text.config(text="Error: " + error_type)
+
     else:
         print(res)
         video_title = res
-        success_text = Label(root, text="Successfully downloaded " + video_title + "!", wraplength=500)
-        success_text.grid(row=6, column=0, columnspan=4)
+        success_text.config(text="Successfully downloaded " + video_title + "!", wraplength=500)
 
 
 title = Label(root, text="YouTube Download", width=40, height=4, font=("Arial", 16))
 title.grid(row=0, column=0, columnspan=4)
+
+download_location = Label(root, text=root.directory)
+download_location.grid(row=3, column=0, columnspan=3)
 
 e = Entry(root, width=60, borderwidth=1)
 e.grid(row=4, column=0, columnspan=3, padx=20, pady=4)
@@ -49,5 +54,10 @@ download_button = Button(root, text="Download", padx=20, pady=10, bg="#ADD8E6", 
 download_button.grid(row=4, column=3)
 
 is_downloading_text = Label(root, text="Downloading...", width=20, height=10)
+success_text = Label(root, text="")
+error_text = Label(root, text="")
+error_text.grid(row=6, column=0, columnspan=4)
+success_text.grid(row=6, column=0, columnspan=4)
+
 
 root.mainloop()
